65c805927075970ca8a623690270c27d6082234d
thasanzadehKamal <kamalhesenzade44@gmail.com>
2026-01-28 21:46:59 +0400

    Improving My Projectgit add .!

---
+++ main.js
@@ -0,0 +1,73 @@
+// Task 1
+//console.log("My name is Klamal");
+
+// Task 2
+//const firstName = "Klamal";
+//console.log(firstName);
+
+// Task 3
+//let user2 = "John"; // Invalid variable name
+//let c@rt = "nigga"; // Invalid variable name
+//let person = "Doe"; // Valid variable name
+//let title color = "mr.santaclaus"; // Invalid variable name
+//let console = "log"; // Invalid variable name
+//let $add = "100"; // Valid variable name
+
+// Task 4
+//let bookAmount = 40;
+//let bookPrice = 200;
+//let totalCost = bookAmount * bookPrice;
+//console.log(totalCost);
+
+// Task 5
+//let kmh = 300;
+//let ms = (kmh * 1000) / 3600;
+//console.log(ms);
+
+// Task 6
+//let bdYear = 4000;
+//let currentYear = new Date();
+//let age = currentYear - bdYear;
+//let daysLived = age * 365;
+//console.log(age);
+
+// Task 7
+//let num = 60;
+//console.log(num ** 3);
+//console.log(num % 3);
+
+// Task 8
+//let firstName = "Charles";
+//let lastName = "Leclerc";
+//console.log(` Sizin adınız ${firstName}  Tanışlığımıza şad deyilem!!! ${lastName}.`);
+
+// Task 10
+//let FullName = "Christian Bale";
+//let spaceIndex = FullName.indexOf(" ");
+//console.log(FullName[4]);
+
+// Task 11
+//let FullName = "Lewis Hamilton";
+//console.log(FullName[0,6]);
+
+// Task 12
+//let fullName = prompt("Enter your full name!!!!");
+//console.log(fullName);
+
+// Task 13
+//var name = prompt("Enter your name:");
+//console.log("Hello, ${name}!");
+
+// Task 14
+//const num1 = parseFloat(prompt("birinci ededi daxil et"));
+//const num2 = parseFloat(prompt("ikinci ededi daxil et"));
+
+//let num = num1 + num2;
+//let num = num1 - num2;
+//let num = num1 * num2;
+//let num = num1 / num2;
+
+//console.log("Toplama: " + (num1 + num2));
+//console.log("Çıxma: " + (num1 - num2));
+//console.log("Vurma: " + (num1 * num2));
+//console.log("Bölmə: " + (num1 / num2)); 
